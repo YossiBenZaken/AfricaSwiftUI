f5a7cc89cb633ee905e330a06dfee5d90eb7bf20
YossiBenZaken <yossibz@gmail.com>
2022-09-27 19:34:29 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Yosef Ben Zaken on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
